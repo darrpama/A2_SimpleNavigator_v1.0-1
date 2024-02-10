18c24e5aca017b38607dd80dedf3ac6f929512d1
v3ssel <v3ssels@pm.me>
2024-02-10 20:51:44 +0300

    export, bfs and dfs to interface

--- src/console/ConsoleInterface.cc
+++ src/console/ConsoleInterface.cc
@@ -1,18 +1,20 @@
+#include <limits>
 #include <iostream>
 
+#include "../graph/s21_graph_algorithms.h"
 #include "ConsoleInterface.h"
 
 namespace s21 {
     void ConsoleInterface::run() {
         helloMsg();
-        int option = 0;
-        while (option != -1) {
-            if (graph == nullptr) {
+        option_ = InterfaceOption::MENU;
+        while (option_ != InterfaceOption::EXIT) {
+            if (graph_ == nullptr) {
                 loadGraph();
                 continue;
             }
-            menuMsg(option);
-            decisionOption(option);
+            menuMsg();
+            decisionOption();
         }
         byeMsg();
     }
@@ -24,32 +26,8 @@ namespace s21 {
         std::cout << "To exit enter -1." << std::endl;
     }
 
-    void ConsoleInterface::loadGraph() {
-        std::cout << "You need to load a graph from adjacency matrix." << std::endl;
-        std::cout << "Please enter a path to file." << std::endl;
-
-        std::string path;
-        std::cin >> path;
-
-        while (path != "-1") {
-            try {
-                auto g = std::make_unique<Graph>();
-                g->LoadGraphFromFile(path);
-                std::cout << "Graph loaded successfully." << std::endl;
-
-                graph = std::move(g);
-                return;
-            } catch (std::exception &e) {
-                std::cout << "An error occured: " << e.what() << std::endl;
-                std::cout << "Please try again or enter -1 to exit." << std::endl;
-                std::cin >> path;
-            }
-        }
-        std::cout << "You denied operation of loading graph." << std::endl;
-    }
-
-    void ConsoleInterface::menuMsg(int& option) const {
-        std::cout << "Please select an option:" << std::endl;
+    void ConsoleInterface::menuMsg() {
+        std::cout << "\nAvailable options:" << std::endl;
         std::cout << "1. Load other graph." << std::endl;
         std::cout << "2. Export the graph to the DOT format." << std::endl;
         std::cout << "3. Depth First Search way of graph." << std::endl;
@@ -64,48 +42,190 @@ namespace s21 {
 
         std::cout << "-1. Exit." << std::endl;
 
-        std::cin >> option;
-        if (option == -1) return;
+        std::cout << "Please select an option." << std::endl;
+        while ((option_ < InterfaceOption::LOAD_GRAPH || option_ > InterfaceOption::TSM_RESEARCH) && option_ != InterfaceOption::EXIT) {
+            int tmp_opt;
+            std::cin >> tmp_opt;
+            if (std::cin.fail()) {
+                std::cin.clear();
+                std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+                std::cout << "Invalid option, please enter correct value." << std::endl;
+                continue;
+            }
 
-        while ((option < 1 || option > 11) && option != -1) {
-            std::cout << "Please enter a valid option." << std::endl;
-            std::cin >> option;
+            option_ = static_cast<InterfaceOption>(tmp_opt);
         }
     }
 
-    void ConsoleInterface::decisionOption(int& option) {
-        switch (option) {
-            case -1:
+    void ConsoleInterface::decisionOption() {
+        switch (option_) {
+            case InterfaceOption::EXIT:
                 return;
 
-            case 1:
+            case InterfaceOption::MENU:
+                return;
+
+            case InterfaceOption::LOAD_GRAPH:
                 loadGraph();
                 break;
+
+            case InterfaceOption::EXPORT_GRAPH:
+                exportGraph();
+                break;
+
+            case InterfaceOption::DFS:
+                dfs();
+                break;
                 
-            case 2:
+            case InterfaceOption::BFS:
+                bfs();
                 break;
-            case 3:
+                
+            case InterfaceOption::DIJKSTRA:
                 break;
-            case 4:
+                
+            case InterfaceOption::FLOYD_WARSHALL:
                 break;
-            case 5:
+                
+            case InterfaceOption::PRIM:
                 break;
-            case 6:
+                
+            case InterfaceOption::ANT_COLONY:
                 break;
-            case 7:
+                
+            case InterfaceOption::TSM2:
                 break;
-            case 8:
+                
+            case InterfaceOption::TSM3:
                 break;
-            case 9:
+                
+            case InterfaceOption::TSM_RESEARCH:
                 break;
-            case 10:
-                break;
-            case 11:
-                break;
+        }
+    }
+
+    void ConsoleInterface::loadGraph() {
+        std::cout << "\n[LOADING GRAPH]" << std::endl;
+        std::cout << "You need to load a graph from adjacency matrix." << std::endl;
+        std::cout << "Please enter a path to file." << std::endl;
+
+        std::string path;
+        std::cin >> path;
+
+        option_ = InterfaceOption::MENU;
+        while (path != std::to_string(static_cast<int>(InterfaceOption::EXIT))) {
+            try {
+                auto g = std::make_unique<Graph>();
+                g->LoadGraphFromFile(path);
+                std::cout << "[SUCCESS] Graph loaded successfully." << std::endl;
+
+                graph_ = std::move(g);
+                return;
+            } catch (std::exception &e) {
+                std::cout << "[FAIL] An error occured: " << e.what() << std::endl;
+                std::cout << "[FAIL] Please try again or enter -1 to exit." << std::endl;
+                std::cin >> path;
+            }
+        }
+
+        std::cout << "You denied operation of loading graph." << std::endl;
+    }
+
+    void ConsoleInterface::exportGraph() {
+        std::cout << "\n[EXPORTING GRAPH]" << std::endl;
+        std::cout << "Please enter a path to file." << std::endl;
+
+        std::string path;
+        std::cin >> path;
+
+        try {
+            graph_->ExportGraphToDot(path);
+        } catch (std::exception &e) {
+            std::cout << "[FAIL] An error occured: " << e.what() << std::endl;
+            std::cout << "[FAIL] Graph has not been exported." << std::endl;
+            return;
+        }
+
+        std::cout << "[SUCCESS] Graph exported successfully." << std::endl;
+        option_ = InterfaceOption::MENU;
+    }
+
+    void ConsoleInterface::dfs() {
+        std::cout << "[DEPTH FIRST SEARCH]" << std::endl;
+        std::cout << "[START VERTEX] ";
+
+        int start_vertex = readVertex();
+        option_ = InterfaceOption::MENU;
+        if (start_vertex == -1) return;
+
+        try {
+            auto way = s21::GraphAlgorithms::DepthFirstSearch(*graph_.get(), start_vertex);
+            std::cout << "[SUCCESS] Depth first search completed successfully." << std::endl;
+            std::cout << "Way of depth first search:" << std::endl;
+            printWay(way);
+            wait();
+        } catch (std::exception &e) {
+            std::cout << "[FAIL] An error occured: " << e.what() << std::endl;
+            std::cout << "[FAIL] Operation denied." << std::endl;
+        }
+    }
+
+    void ConsoleInterface::bfs() {
+        std::cout << "[BREADTH FIRST SEARCH]" << std::endl;
+        std::cout << "[START VERTEX] ";
+        
+        int start_vertex = readVertex();
+        option_ = InterfaceOption::MENU;
+        if (start_vertex == -1) return;
+
+        try {
+            auto way = s21::GraphAlgorithms::BreadthFirstSearch(*graph_.get(), start_vertex);
+            std::cout << "[SUCCESS] Breadth first search completed successfully." << std::endl;
+            std::cout << "Way of breadth first search:" << std::endl;
+            printWay(way);
+            wait();
+        } catch (std::exception &e) {
+            std::cout << "[FAIL] An error occured: " << e.what() << std::endl;
+            std::cout << "[FAIL] Operation denied." << std::endl;
         }
     }
 
     void ConsoleInterface::byeMsg() const {
         std::cout << "Goodbye!" << std::endl;
+    }
+    
+    int ConsoleInterface::readVertex() {
+        std::cout << "Please enter a vertex." << std::endl;
+
+        int start_vertex;
+        std::cin >> start_vertex;
+
+        if (std::cin.fail()) {
+            std::cin.clear();
+            std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+            std::cout << "[FAIL] Invalid vertex value, operation denied." << std::endl;
+
+            return -1;
+        }
+
+        return start_vertex;
+    }
+    
+    void ConsoleInterface::printWay(std::vector<int> &way) {
+        for (size_t i = 0; i < graph_->size() - 1; i++) {
+            std::cout << way[i] << " -> ";
+        }
+
+        std::cout << way.back() << std::endl;
+    }
+    
+    void ConsoleInterface::wait() {
+        std::cout << "Press enter to continue..." << std::endl;
+
+        std::cin.clear();
+        std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+        
+        std::string line;
+        std::getline(std::cin, line);
     }
 }
--- src/console/ConsoleInterface.h
+++ src/console/ConsoleInterface.h
@@ -7,18 +7,52 @@
 namespace s21 {
     class ConsoleInterface {
        public:
+        enum class InterfaceOption {
+            EXIT = -1,
+            MENU = 0,
+            LOAD_GRAPH = 1,
+            EXPORT_GRAPH = 2,
+            DFS = 3,
+            BFS = 4,
+            DIJKSTRA = 5,
+            FLOYD_WARSHALL = 6,
+            PRIM = 7,
+            ANT_COLONY = 8,
+            TSM2 = 9,
+            TSM3 = 10,
+            TSM_RESEARCH = 11
+        };
+
         void run();
 
         virtual void helloMsg() const;
         
+        virtual void menuMsg();
+        virtual void decisionOption();
+        
         virtual void loadGraph();
-        virtual void menuMsg(int& option) const;
-        virtual void decisionOption(int& option);
+        virtual void exportGraph();
+        virtual void dfs();
+        virtual void bfs();
+        // virtual void dijkstra();
+        // virtual void floydWarshall();
+        // virtual void prim();
+        // virtual void antColony();
+
+        // virtual void tsm2();
+        // virtual void tsm3();
+        // virtual void tsmResearch();
 
         virtual void byeMsg() const;
 
        protected:
-        std::unique_ptr<Graph> graph;
+        std::unique_ptr<Graph> graph_;
+        InterfaceOption option_;
+    
+       private:
+        int readVertex();
+        void printWay(std::vector<int> &way);
+        void wait();
     };
 } // namespace s21
 
